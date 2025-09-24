61e65e7f5f16505b157fa8676fb1c63b6be11741
MiFr <110558561+Mikester58@users.noreply.github.com>
2025-09-23 20:14:54 -0500

    Make run function for Agent demo prettier

--- TheJoyOfTesting/AgentDemo.py
+++ TheJoyOfTesting/AgentDemo.py
@@ -1,6 +1,5 @@
 import os
 import requests
-import json
 import sys
 
 API_KEY = os.getenv("TAMU_API_KEY")
@@ -19,7 +18,7 @@ body = {
     "model": "protected.llama3.2",
     "stream": False,
     "messages": [
-        {"role": "user", "content": "Explain Ohms law to me in great detail and how it works en tandum with Kirchoffs laws"}
+        {"role": "user", "content": "What are florida beaches"}
     ],
 }
 
@@ -32,4 +31,4 @@ resp2 = requests.post(chat_url, headers=chat_headers, json=body, timeout=30)
 
 print("\n=== Chat Completion ===")
 print("Status:", resp2.status_code)
-print(json.dumps(resp2.json(), indent=2))
+print(resp2.json()["choices"][0]["message"]["content"])
