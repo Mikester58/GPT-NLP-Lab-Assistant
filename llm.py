5a84400f0c8936c8f0e756e78b36b67f7152e412
MiFr <110558561+Mikester58@users.noreply.github.com>
2025-10-31 17:21:27 -0500

    Updated llm.py
    Revamped to contain more useful functions.

--- llm.py
+++ llm.py
@@ -2,8 +2,12 @@
 File to handle the Large Language Model being ran on-device.
 
 Provides:
-- GetChatHistory(llm, db) -> Callable
-- GetStreamHistory(llm, db) -> Callable
+- GetChatHistory(llm, db, session_id) -> Callable
+- GetStreamHistory(llm, db, session_id) -> Callable
+- ClearMemory(session_id) -> None
+- GetMemoryMessages(session_id) -> List[BaseMessage]
+- ListSessions -> List[str]
+- GetSessionHistory(session_id) -> BaseChatMessageHistory
 """
 from typing import Dict, List
 from langchain_core.chat_history import BaseChatMessageHistory, InMemoryChatMessageHistory
@@ -15,4 +19,38 @@ from langchain_core.output_parsers import StrOutputParser
 from database_bridge import CombineDocuments
 from config import ANSWER_PROMPT_TMP, DEFAULT_DOC_PROMPT, RETRIEVER_K
 
+#Store for conversation history
 store: Dict[str, InMemoryChatMessageHistory] = {}
+
+def GetChatHistory(llm, db, session_id: str = "default"):
+
+
+def GetStreamHistory(llm, db, session_id: str = "default"):
+
+
+#Helper functions
+
+#Clear conversation memory for a specific session
+def ClearMemory(session_id: str = "default") -> None:
+    if session_id in store:
+        store[session_id].clear()
+    return None
+
+#Return messages from conversation session
+def GetMemoryMessages(session_id: str = "default") -> List[BaseMessage]:
+    if session_id in store:
+        return store[session_id].messages
+    else:
+        return []
+
+#Return a list of active session IDs
+def ListSessions() -> List[str]:
+    return list(store.keys())
+
+#Return/Create & Return a chat message history for a given chat
+def GetSessionHistory(session_id: str) -> BaseChatMessageHistory:
+    if session_id in store:
+        return store[session_id]
+    else:
+        store[session_id] = InMemoryChatMessageHistory()
+        return store[session_id]
